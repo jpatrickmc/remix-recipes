88f5661a32593cbebbe3fd0c7402e760aaa2b8a5
Patrick McDaniel <dfwjpm@outlook.com>
2024-07-23 16:56:07 -0500

    fix loader return object

--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -4,7 +4,7 @@ import { getAllShelves } from "./models/pantry-shelf.server";
 
 export async function loader() {
   const shelves = await getAllShelves();
-  return json(shelves);
+  return json({ shelves });
 }
 
 export default function Pantry() {
@@ -13,7 +13,7 @@ export default function Pantry() {
     <div>
       <h1>Welcome to the pantry :)</h1>
       <ul>
-        {data.map((shelf) => (
+        {data.shelves.map((shelf) => (
           <li key={shelf.id}>{shelf.name}</li>
         ))}
       </ul>
