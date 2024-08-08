428f1bedabb8328bcc5cd4a38fd8eead7d08e40e
Patrick McDaniel <dfwjpm@outlook.com>
2024-08-08 11:41:26 -0500

    add login route

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,27 @@
+import { ActionFunction, json } from "@remix-run/node";
 import classNames from "classnames";
+import { z } from "zod";
 import { ErrorMessage, PrimaryButton } from "~/components/forms";
+import { validateForm } from "./utils/validation";
+import { useActionData } from "@remix-run/react";
+
+const loginSchema = z.object({
+  email: z.string().email(),
+});
+
+export const action: ActionFunction = async ({ request }) => {
+  const formdata = await request.formData();
+
+  return validateForm(
+    formdata,
+    loginSchema,
+    ({ email }) => {},
+    (errors) => json({ errors, email: formdata.get("email") }, { status: 400 })
+  );
+};
 
 export default function Login() {
+  const actionData = useActionData();
   return (
     <div className="text-center mt-3">
       <h1 className="text-3xl mb-8">Remix Recipes</h1>
@@ -12,12 +32,13 @@ export default function Login() {
             name="email"
             placeholder="Email"
             autoComplete="off"
+            defaultValue={actionData?.email}
             className={classNames(
               "w-full outline-none border-2 border-gray-200",
               "focus:border-primary rounded-md p-2"
             )}
           />
-          <ErrorMessage></ErrorMessage>
+          <ErrorMessage>{actionData?.errors?.email}</ErrorMessage>
         </div>
         <PrimaryButton className="w-1/2 mx-auto">Log In</PrimaryButton>
       </form>
